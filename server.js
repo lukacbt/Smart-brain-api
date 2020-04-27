f1b727a940d587ade8eca7ec5396abf562b7f354
Luka Kukina <lukscbt@gmail.com>
2020-04-27 08:59:40 +0200

    connectionString

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const register = require("./Controllers/register")
 const signin = require("./Controllers/signin")
 const profile = require("./Controllers/profile")
 const image = require("./Controllers/image")
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
 	client: 'pg',
-	connection: {
+	connectionString: {
 		connectionString: process.env.DATABASE_URL,
   		ssl: true,
 	}
